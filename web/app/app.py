69e5065db767b164e42e365d17646cdb970e6e39
Vidal Alvarez Manuel <valvarezma@unsa.edu.pe>
2025-02-05 08:30:56 -0500

    actualizando cambios

--- web/app/app.py
+++ web/app/app.py
@@ -120,6 +120,6 @@ def checkout():
         session.pop('cart', None)  # Limpiar el carrito
         return redirect(url_for('index'))
     return render_template('checkout.html')
-
+#inicio de la aplicación
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
